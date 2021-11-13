c4871478489f86275e4a4b02c774c0257f0d0204
Martin Herrmann <martin@famherrmann.de>
2021-11-13 17:06:34 +0100

    Add --verbose

--- wekan-db.py
+++ wekan-db.py
@@ -12,6 +12,7 @@ app = typer.Typer()
 
 server_host = "localhost"
 server_port = 27019
+verbose = False
 
 
 class ADict(AttrDict):
@@ -20,26 +21,34 @@ class ADict(AttrDict):
         return self["_id"]
 
 
+def vprint(*args, **kwargs):
+    if verbose:
+        print(*args, **kwargs)
+
+
 @app.callback()
-def callback(host: str = "localhost", port: int = 27017):
+def callback(host: str = "localhost", port: int = 27017,
+             verbose_: bool = typer.Option(False, "--verbose", "-v")):
     global server_host
     global server_port
+    global verbose
 
     server_host = host
     server_port = port
+    verbose = verbose_
 
 
 def find_boards(boards: Collection, title: str) -> List[AttrDict]:
-    print(f"Finding board {title!r}...", end="")
+    vprint(f"Finding board {title!r}...", end="")
     result = list(boards.find({"archived": False, "title": title}))
-    print(", ".join(board["_id"] for board in result))
+    vprint(", ".join(board["_id"] for board in result))
     return result
 
 
 def find_lists(lists: Collection, board_id: str, title: str) -> List[AttrDict]:
-    print(f"Finding list {title!r} in board {board_id}...", end="")
+    vprint(f"Finding list {title!r} in board {board_id}...", end="")
     result = list(lists.find({"archived": False, "boardId": board_id, "title": title}))
-    print(", ".join(document["_id"] for document in result))
+    vprint(", ".join(document["_id"] for document in result))
     return result
 
 
@@ -51,13 +60,16 @@ def find_boards_lists(boards: Collection, lists: Collection, board_title: str, l
 
 
 def count_cards(cards: Collection, list_id: str) -> int:
-    return cards.count_documents({"archived": False, "listId": list_id})
+    vprint(f"Counting cards in list {list_id}...", end="")
+    result = cards.count_documents({"archived": False, "listId": list_id})
+    vprint(f"{result}")
+    return result
 
 
 def find_swimlane(swimlanes: Collection, board_id: str) -> Optional[AttrDict]:
-    print(f"Finding a swimlane in board {board_id}...", end="")
+    vprint(f"Finding a swimlane in board {board_id}...", end="")
     result = swimlanes.find_one({"archived": False, "boardId": board_id})
-    print(result["_id"] if result is not None else "not found")
+    vprint(result["_id"] if result is not None else "not found")
     return result
 
 
@@ -95,12 +107,14 @@ def move_cards(from_board_title: str, from_list_title: str, to_board_title: str,
 
     total_moved = 0
     for from_board, from_list in from_boards_lists:
+        vprint(f"Moving cards from list {from_list.id} in board {from_board.id}...", end="")
         result = db["cards"].update_many(filter={"archived": False,
                                                  "boardId": from_board.id,
                                                  "listId": from_list.id},
                                          update={"$set": {"boardId": to_board.id,
                                                           "swimlaneId": to_swimlane.id,
                                                           "listId": to_list.id}})
+        vprint(f"{result.modified_count} moved")
         total_moved += result.modified_count
 
     print(f"{total_moved} cards moved, {existing_card_count} already there")
